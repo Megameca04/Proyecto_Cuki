033449d5ad2ac2c7a8eb4aceba0e806b8c179409
Megameca04 <megameca04@gmail.com>
2023-04-12 09:03:38 -0500

    Explosiones de barriles arregladas

--- Cuki-Clash of the Kuins/Scripts/Barril_b.gd
+++ Cuki-Clash of the Kuins/Scripts/Barril_b.gd
@@ -22,12 +22,16 @@ func _on_hitbox_area_entered(area):
 		still = false
 		$AnimationPlayer.play("Rodar")
 		movement = -Vector2(cos(get_angle_to(area.global_position)),sin(get_angle_to(area.global_position)))
-
+	if area.is_in_group("expl_attack"):
+		blow_up()
 
 func _on_hitbox_body_entered(body):
 	if !still:
-		var expl = EXPL.instantiate()
-		expl.global_position = self.global_position
-		call_deferred("add_sibling",expl)
-		queue_free()
+		blow_up()
+
+func blow_up():
+	var expl = EXPL.instantiate()
+	expl.global_position = self.global_position
+	call_deferred("add_sibling",expl)
+	queue_free()
 	
