4405dcc83344a369cac57beee05f9a0d7c2838ee
Hollowriter <deadgamergx2@hotmail.com.ar>
2023-11-05 17:28:35 -0300

    uploading testing totem

--- Cuki-Clash of the Kuins/Scripts/Totem.gd
+++ Cuki-Clash of the Kuins/Scripts/Totem.gd
@@ -3,31 +3,29 @@ extends Area2D
 enum DifficultyLevels { EASY, NORMAL, HARD }
 @export var difficulty:DifficultyLevels
 @export var rounds = 0
-@export var easyEnemiesDictionary = {}
+@onready var rabion = preload("res://Entidades/Rabion.tscn")
+@onready var blonk = preload("res://Entidades/blonk.tscn")
+@onready var chooty = preload("res://Entidades/Chooty.tscn")
+@onready var explosive_eater = preload("res://Entidades/explosive_eater.tscn")
+var easyEnemiesDictionary = {0:"Rabion", 1:"Blonk", 2:"Chooty", 3:"Explosive_Eater"}
 @export var normalEnemiesDictionary = {}
 @export var hardEnemiesDictionary = {}
 var enemyStates = {0:"Freeze", 1:"Poison", 2:"Flame", 3:"Water", 4:"Tar", 5:"Shock"}
 var randomQuantityEnemies = 0
 var randomEnemyIndex = 0
 var randomEnemyState = 0
+var Cuki = null
 
-func detectPlayer(groupName):
-	if (groupName == "Player"):
+func detectPlayer(player):
+	if (player.name == "Cuki"):
+		Cuki = player
 		generateEnemies()
 
-func detectEnemies(groupName):
-	if (groupName == "Enemy"):
-		pass
-
-func detectBarrels(groupName):
-	if (groupName == "Barrels"):
-		pass
-
 func generateEnemies():
-	pass
+	var enemy = rabion.instantiate()
+	enemy.global_position = Vector2(0, 0)
+	enemy.global_position = self.global_position
+	#self.call_deferred("add_child", enemy)
 
 func _on_body_entered(body):
-	for i in body.get_groups():
-		detectPlayer(i)
-		detectEnemies(i)
-		detectBarrels(i)
+	detectPlayer(body)
